3740ded40fc70048ffe1ca5e40950aeb48e6202d
hasoy <63263034+hasoy@users.noreply.github.com>
2023-02-09 15:07:12 +0100

    v modal week select

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
   <v-container fluid>
     <img alt="Clarify logo" src="./assets/logo.png" height="100" />
     <v-select
+      v-model="select"
       :items="weekNumbers"
       :menu-props="{ top: true, offsetY: true }"
       label="Weeknumber"
     ></v-select>
-    <ComboBox v-bind:tasks="tasks" />
-    Example of a week
+    <ComboBox v-bind:tasks="tasks" weekNumber="" />
+    Example of a week {{ select }}
     <Periods />
   </v-container>
 </template>
@@ -25,6 +26,7 @@ export default {
   data: () => ({
     tab: null,
     weekNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+    select: null,
     tasks: [
       "105781 - Boels",
       "100213 - Vacation",
