bd175659f804c6f38d77e339f40f50a354b3ba72
Daniel <daniel.ten.napel@capgemini.com>
2023-02-09 13:01:09 +0100

    first draft of week and day component
    first draft of week and day component

---
+++ src/components/DayComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div class="day">{{ day }}</div>
+    <div class="date">{{ date }}</div>
+    <input type="number" min="0" value="hours">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DayComponent',
+  props: {
+    day: String,
+    date: String,
+    hours: String
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  background-color: #f1f1f1;
+  width: 100px;
+  margin: 10px;
+  text-align: center;
+  line-height: 75px;
+  font-size: 30px;
+}
+.day {
+  font-size: 20px;
+}
+.date {
+  font-size: 10px;
+}
+input {
+  background-color: #f1f1f1;
+  width: 50px;
+  margin: 10px;
+  text-align: center;
+  line-height: 75px;
+  font-size: 30px;
+}
+</style>
---
+++ src/components/WeekComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="week-container">
+    <DayComponent day="Mon" date="01-01-2023" hours="0"/>
+    <DayComponent day="Mon" date="01-01-2023" hours="0"/>
+    <DayComponent day="Mon" date="01-01-2023" hours="0"/>
+    <DayComponent day="Mon" date="01-01-2023" hours="0"/>
+    <DayComponent day="Mon" date="01-01-2023" hours="0"/>
+    <DayComponent day="Mon" date="01-01-2023" hours="0"/>
+    <DayComponent day="Mon" date="01-01-2023" hours="1"/>
+  </div>
+</template>
+
+<script>
+import DayComponent from './DayComponent.vue'
+
+export default {
+  name: 'WeekComponent',
+  components: {
+    DayComponent,
+},
+  props: {
+    msg: String,
+    day: String,
+    hours: String
+  }
+}
+</script>
+
+<style scoped>
+.week-container {
+  display: flex;
+  flex-wrap: nowrap;
+  background-color: DodgerBlue;
+}
+</style>
