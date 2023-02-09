02a5015da26484b244311af0b0a8b1eaee68b99f
hasoy <63263034+hasoy@users.noreply.github.com>
2023-02-09 14:07:31 +0100

    general overview for weeks/months/quarter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,82 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Clarity</h1>
+    <div class="container">
+      <button
+        @click="
+          {
+            this.weeksView = !this.weeksView;
+          }
+        "
+      >
+        weeks
+      </button>
+      <button
+        @click="
+          {
+            this.monthsView = !this.monthsView;
+          }
+        "
+      >
+        month
+      </button>
+      <button
+        @click="
+          {
+            this.quartersView = !this.quartersView;
+          }
+        "
+      >
+        quarter
+      </button>
+
+      <select id="weeks" name="weeks">
+        <option v-for="week in weeks" :key="week">
+          {{ week }}
+        </option>
+      </select>
+
+      <div v-if="weeksView">
+        <WeekTable :dates="dates[0]"></WeekTable>
+      </div>
+
+      <button>add task</button>
+    </div>
   </div>
 </template>
 
 <script>
+import WeekTable from "./WeekTable.vue";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      dates: [
+        {
+          weekNumber: 1,
+          hours: [8, 8, 8, 8, 8, 8, 8],
+          date: new Date(),
+          projectCode: "x",
+        },
+        {
+          weekNumber: 2,
+          hours: [8, 8, 8, 8, 8, 8, 8],
+          date: new Date(),
+          projectCode: "y",
+        },
+      ],
+      weeksView: true,
+      monthsView: false,
+      quartersView: false,
+    };
+  },
+  props: {},
+  components: {
+    WeekTable,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/components/WeekTable.vue
@@ -0,0 +1,59 @@
+<template>
+  <table>
+    <thead>
+      <!-- for calculating the date and adding it as table header -->
+      <!-- <td>month</td>
+      <td v-for="day in days" :key="{ day }">
+        {{}}
+        {{ getDate(newDates.indexOf(day.weekNumber)) }}
+      </td> -->
+    </thead>
+    <tbody>
+      <tr>
+        <td>hours</td>
+        <td v-for="(hour, index) in dates.hours" :key="hour.date">
+          <input type="number" v-model="newDates.hours[index]" />
+        </td>
+      </tr>
+    </tbody>
+    <button @click="logHours()">log Hours</button>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "WeekTable",
+  props: {
+    dates: Array,
+  },
+  data() {
+    return {
+      newDates: this.dates,
+      days: [1, 2, 3, 4, 5, 6, 7],
+    };
+  },
+
+  methods: {
+    getMonth: function () {
+      let date = new Date();
+      return new Intl.DateTimeFormat("en-EN", { month: "long" }).format(date);
+    },
+    getDay: function (offset) {
+      let date = new Date();
+      date.setDate(date.getDate() + offset);
+      return new Intl.DateTimeFormat("en-EN", { weekday: "long" }).format(date);
+    },
+    getDate: function (offset) {
+      let date = new Date();
+      let day = date.getDate() + offset;
+      let month = date.getMonth() + 1;
+      return `${day}-${month}`;
+    },
+    logHours: function () {
+      console.log(this.newDates);
+    },
+  },
+};
+</script>
+
+<style></style>
