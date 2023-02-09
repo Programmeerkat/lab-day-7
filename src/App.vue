af99f3bc4ad075706787693136ffc1e2e4261724
hasoy <63263034+hasoy@users.noreply.github.com>
2023-02-09 14:50:38 +0100

    added weekselector

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <v-container fluid>
     <img alt="Clarify logo" src="./assets/logo.png" height="100" />
+    <v-select
+      :items="weekNumbers"
+      :menu-props="{ top: true, offsetY: true }"
+      label="Weeknumber"
+    ></v-select>
     <ComboBox v-bind:tasks="tasks" />
-    Example of  a week
+    Weeknumber
     <Periods />
   </v-container>
 </template>
 
 <script>
-import Periods from './components/Periods.vue'
+import Periods from "./components/Periods.vue";
 import ComboBox from "./components/ComboBox.vue";
 
 export default {
@@ -18,17 +23,18 @@ export default {
     ComboBox,
   },
   data: () => ({
-      tab: null,
-      tasks: [
-        "105781 - Boels",
-        "100213 - Vacation",
-        "100123 - Doctor",
-        "100622 - Dentist",
-        "100101 - Availability",
-        "100523 - Education"
-      ]
-    }),
-}
+    tab: null,
+    weekNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+    tasks: [
+      "105781 - Boels",
+      "100213 - Vacation",
+      "100123 - Doctor",
+      "100622 - Dentist",
+      "100101 - Availability",
+      "100523 - Education",
+    ],
+  }),
+};
 </script>
 
 <style>
