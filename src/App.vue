7af97ec85be4eebaa3a96f548d6a28d723736bb6
Thomas Kraak <thomas.kraak@capgemini.com>
2023-02-09 14:30:04 +0100

    Updated favicon and font family

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,38 @@
   <v-container fluid>
     <img alt="Clarify logo" :src="require('./assets/logo.png')" height="100" />
     <ComboBox v-bind:tasks="tasks" />
-    Example of  a week
+    <p>Example of a week</p>
     <Periods />
   </v-container>
 </template>
 
 <script>
-import Periods from './components/Periods.vue'
+import Periods from "./components/Periods.vue";
 import ComboBox from "./components/ComboBox.vue";
 
 export default {
   name: "App",
   components: {
     Periods,
-    ComboBox,
+    ComboBox
   },
   data: () => ({
-      tab: null,
-      tasks: [
-        "105781 - Boels",
-        "100213 - Vacation",
-        "100123 - Doctor",
-        "100622 - Dentist",
-        "100101 - Availability",
-        "100523 - Education"
-      ]
-    }),
-}
+    tab: null,
+    tasks: [
+      "105781 - Boels",
+      "100213 - Vacation",
+      "100123 - Doctor",
+      "100622 - Dentist",
+      "100101 - Availability",
+      "100523 - Education"
+    ]
+  })
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Calibri, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
